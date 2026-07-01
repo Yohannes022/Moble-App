7422fa17c64591fead22dc3d51fdf18f5a8d0013
Yohannes022 <johannest022@gmail.com>
2026-07-01 12:04:13 +0300

    Update app changes

---
+++ src/app/(auth)/sign-in.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { Link } from 'expo-router'
+
+const SignIn = () => {
+  return (
+    <View>
+      <Text>SignIn</Text>
+      <Link href="/(auth)/sign-up">Create Account</Link>
+    </View>
+  )
+}
+
+export default SignIn
+
+const styles = StyleSheet.create({})
---
+++ src/app/(auth)/sign-up.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { Link } from 'expo-router'
+
+const SignUp = () => {
+  return (
+    <View>
+      <Text>SignUp</Text>
+      <Link href="/(auth)/sign-up">Sign In</Link>
+    </View>
+  )
+}
+
+export default SignUp
+
+const styles = StyleSheet.create({})
